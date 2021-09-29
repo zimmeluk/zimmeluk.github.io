78428e574a18cd1dd9c5525625c6d663122f6719
Luke Zimmerman <zimmeluk14@gmail.com>
2021-09-28 21:22:44 -0500

    add tailwindcss config, modify components to use className instead of class, remove tailwind, remove default content

--- next_js_app/nextjs-blog/components/header.js
+++ next_js_app/nextjs-blog/components/header.js
@@ -1,36 +1,36 @@
 import Link from 'next/link'
 
 export default function Header() {
-    return(
-        // Fixed navbar
-        <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                    </button>
-                    <a class="navbar-brand" href="/">Luke Zimmerman</a>
-                </div>
-                <div id="navbar" class="navbar-collapse collapse">
-                     <ul class="nav navbar-nav navbar-right">
-                        <li>
-                            <Link href={'/about'}>
-                                <a>About</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={'/projects'}>
-                                <a>Projects</a>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href={'/blog'}>
-                                <a>Blog</a>
-                            </Link>
-                        </li>
-                     </ul>
-                </div>
-            </div>
-        </nav>
-    )
+  return(
+    // Fixed navbar
+    <nav className="flex w-screen bg-">
+      <div className="container">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span className="sr-only">Toggle navigation</span>
+          </button>
+          <a className="text-xl" href="/">Luke Zimmerman</a>
+        </div>
+        <div id="navbar" className="navbar-collapse collapse">
+          <ul className="flex flex-row space-x-4">
+            <li>
+              <Link href={'/about'}>
+                  <a className="text-gray-500 hover:text-gray-800">About</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/projects'}>
+                  <a className="text-gray-500 hover:text-gray-800">Projects</a>
+              </Link>
+            </li>
+            <li>
+              <Link href={'/blog'}>
+                  <a className="text-gray-500 hover:text-gray-800">Blog</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
--- next_js_app/nextjs-blog/pages/_app.js
+++ next_js_app/nextjs-blog/pages/_app.js
@@ -1,7 +1,18 @@
 import 'tailwindcss/tailwind.css'
+import '../styles/globals.css'
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <div>
+            <Header />
+
+            <Component {...pageProps} />
+
+            <Footer />
+        </div>
+    )
 }
 
 export default MyApp
