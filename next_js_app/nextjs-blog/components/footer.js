a029cedcf03759874dc56f40c3c6360e3b6d3133
Luke Zimmerman <zimmeluk14@gmail.com>
2021-09-30 21:30:59 -0500

    add additional styling to nav bar, remove assets and replace with react-icons

--- next_js_app/nextjs-blog/components/footer.js
+++ next_js_app/nextjs-blog/components/footer.js
@@ -1,12 +1,26 @@
 import Link from 'next/link'
+import { FaTwitter, FaFacebook, FaLinkedin, FaGithub } from 'react-icons/fa';
 
 export default function Footer() {
-    return(
-        <footer>
-            <a href="https://twitter.com/zimmeluk"><img border="0" alt="Twitter" src="/assets/TwitterGray.png" width="32" height="32" /></a>
-            <a href="https://www.facebook.com/luke.zimmerman.14661"><img border="0" alt="Facebook" src="/assets/FacebookGray.png" width="32" height="32" /></a>
-            <a href="https://www.linkedin.com/in/lukedzimmerman/"><img border="0" alt="LinkedIn" src="/assets/LinkedinGray.png" width="32" height="32" /></a>
-            <a href="https://github.com/zimmeluk"><img border="0" alt="GitHub" src="/assets/GithubGray.png" width="32" height="32" /></a>
-        </footer>
-    )
+  return(
+    <footer className="flex flex-row">
+      <a href="https://twitter.com/zimmeluk"><img border="0" alt="Twitter" src="/assets/TwitterGray.png" width="32" height="32" /></a>
+      <a href="https://www.facebook.com/luke.zimmerman.14661"><img border="0" alt="Facebook" src="/assets/FacebookGray.png" width="32" height="32" /></a>
+      <a href="https://www.linkedin.com/in/lukedzimmerman/"><img border="0" alt="LinkedIn" src="/assets/LinkedinGray.png" width="32" height="32" /></a>
+      <a href="https://github.com/zimmeluk"><img border="0" alt="GitHub" src="/assets/GithubGray.png" width="32" height="32" /></a>
+
+      <Link href={'https://twitter.com/zimmeluk'}>
+        <FaTwitter size={32} />
+      </Link>
+      <Link href={'https://www.facebook.com/luke.zimmerman.14661'}>
+        <FaFacebook size={32} />
+      </Link>
+      <Link href={'https://www.linkedin.com/in/lukedzimmerman/'}>
+        <FaLinkedin size={32} />
+      </Link>
+      <Link href={'https://github.com/zimmeluk'}>
+        <FaGithub size={32} />
+      </Link>
+    </footer>
+  )
 }
--- next_js_app/nextjs-blog/components/header.js
+++ next_js_app/nextjs-blog/components/header.js
@@ -1,35 +1,41 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { IoIosSearch } from 'react-icons/io';
 
 export default function Header() {
   return(
-    // Fixed navbar
-    <nav className="flex w-screen bg-">
-      <div className="container">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span className="sr-only">Toggle navigation</span>
-          </button>
-          <a className="text-xl" href="/">Luke Zimmerman</a>
-        </div>
-        <div id="navbar" className="navbar-collapse collapse">
-          <ul className="flex flex-row space-x-4">
-            <li>
-              <Link href={'/about'}>
-                  <a className="text-gray-500 hover:text-gray-800">About</a>
-              </Link>
-            </li>
-            <li>
-              <Link href={'/projects'}>
-                  <a className="text-gray-500 hover:text-gray-800">Projects</a>
-              </Link>
-            </li>
-            <li>
-              <Link href={'/blog'}>
-                  <a className="text-gray-500 hover:text-gray-800">Blog</a>
-              </Link>
-            </li>
-          </ul>
-        </div>
+    <nav className="flex items-center w-screen bg-yellow-400">
+      <div className="flex-1 md:hidden">
+        <button id="navExpand">
+        </button>
+      </div>
+
+      <div id="navbar" className="hidden flex-1 md:flex justify-center navbar-collapse collapse">
+        <ul className="flex flex-row space-x-4">
+          <li>
+            <Link href={'/projects'}>
+                <a className="text-gray-500 hover:text-gray-800">Projects</a>
+            </Link>
+          </li>
+          <li>
+            <Link href={'/blog'}>
+                <a className="text-gray-500 hover:text-gray-800">Blog</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <div className="flex-1 md:flex justify-center">
+        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span className="sr-only">Toggle navigation</span>
+        </button>
+        <a className="text-xl" href="/">Luke Zimmerman</a>
+      </div>
+
+      <div className="flex flex-1 flex-row-reverse">
+        <span>
+          <IoIosSearch size={16}/>
+        </span>
+        <input className="mr-5 rounded-md" type="text" placeholder="Search..." />
       </div>
     </nav>
   )
